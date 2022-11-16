8b9e96ebd48f0784b0cda8ee8901544019084fef
devwael <dev.ahmedwael@gmail.com>
2022-11-17 00:03:01 +0200

    fix check user for allowed shop order

--- includes/class-wc-shop-orders-registration.php
+++ includes/class-wc-shop-orders-registration.php
@@ -40,7 +40,7 @@ class Wc_Shop_Orders_Gateway_Registration {
 	 */
 	public function allowed_shop_order_users( $available_gateways ) {
 		$user_id             = get_current_user_id();
-		$can_use_shop_orders = get_user_meta( $user_id, 'wcso_can_user_shop_orders', true ) === 1;
+		$can_use_shop_orders = get_user_meta( $user_id, 'wcso_can_user_shop_orders', true ) === 'yes';
 		if ( isset( $available_gateways['wc-shop-orders'] ) && ! $can_use_shop_orders ) {
 			unset( $available_gateways['wc-shop-orders'] );
 		}
