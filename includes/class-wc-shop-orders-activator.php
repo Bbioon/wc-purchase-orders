ef337c5ad731009411b5d88837a05d2040fafabe
devwael <dev.ahmedwael@gmail.com>
2022-11-16 19:28:11 +0200

    initial files

---
+++ includes/class-wc-shop-orders-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bbioon.com
+ * @since      1.0.0
+ *
+ * @package    Wc_Shop_Orders
+ * @subpackage Wc_Shop_Orders/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wc_Shop_Orders
+ * @subpackage Wc_Shop_Orders/includes
+ * @author     Ahmad Wael <dev.ahmedwael@gmail.com>
+ */
+class Wc_Shop_Orders_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wc-shop-orders-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bbioon.com
+ * @since      1.0.0
+ *
+ * @package    Wc_Shop_Orders
+ * @subpackage Wc_Shop_Orders/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wc_Shop_Orders
+ * @subpackage Wc_Shop_Orders/includes
+ * @author     Ahmad Wael <dev.ahmedwael@gmail.com>
+ */
+class Wc_Shop_Orders_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
