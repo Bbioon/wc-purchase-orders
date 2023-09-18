057318ecea7b1755b8120f1fb9a8278e017bbdc5
devwael <dev.ahmedwael@gmail.com>
2023-09-18 17:41:25 +0300

    Disallowing Direct File Access to plugin files

--- includes/class-wc-purchase-orders-files.php
+++ includes/class-wc-purchase-orders-files.php
@@ -1,5 +1,5 @@
 <?php
-
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * The purchase orders file handler.
  *
--- public/class-wc-purchase-orders-public.php
+++ public/class-wc-purchase-orders-public.php
@@ -1,5 +1,5 @@
 <?php
-
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * The public-facing functionality of the plugin.
  *
